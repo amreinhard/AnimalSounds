8491d32bdb2b8b8ef7c108e835dafdf5a5b30dba
Amanda <amreinhard@icloud.com>
2018-03-07 18:37:39 -0800

    Completed project.

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let meowSound = SimpleSound(named:"meow")
+    let woofSound = SimpleSound(named:"woof")
+    let mooSound = SimpleSound(named:"moo")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
+    @IBAction func catButtonTapped(_ sender: Any) {
+        animalSoundLabel.text = "Meow!"
+        meowSound.play()
+    }
+    @IBAction func dogButtonTapped(_ sender: Any) {
+        animalSoundLabel.text = "Woof!"
+        woofSound.play()
+    }
+    @IBAction func cowButtonTapped(_ sender: Any) {
+        animalSoundLabel.text = "Moo!"
+        mooSound.play()
+    }
 }
 
